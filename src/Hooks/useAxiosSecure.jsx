1a26777c9983d3346b86a6cc24fce5bbbac74fef
OHshajim <ajshajimmax@gmail.com>
2024-06-04 23:04:19 +0600

    secured bugs fix

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,27 +4,25 @@ const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
-    axios.interceptors.request.use(function(config){
+    axiosSecure.interceptors.request.use(function (config) {
         const token = localStorage.getItem('access_token')
-        console.log(token);
-        // config.headers.authorization = `bearer ${token}`
-        console.log(config.headers);
+        config.headers.authorization = `bearer ${token}`
         return config
-    }, function(error){
+    }, function (error) {
         // Do something with request error
         return Promise.reject(error);
-        });
+    });
 
-    // axios.interceptors.response.use(function(response){
-    //     return response;
-    // },
-    //     async (error) => {
-    //         const status = error.response.status;
-    //         console.log('error in interceptor', status);
+    axios.interceptors.response.use(function (response) {
+        return response;
+    },
+        async (error) => {
+            const status = error.response.status;
+            console.log('error in interceptor', status);
 
-    //         return Promise.reject(error);
-    //     }
-    // )
+            return Promise.reject(error);
+        }
+    )
     return axiosSecure;
 };
 
