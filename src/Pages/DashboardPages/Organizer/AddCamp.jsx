c578e7521a3a8d0f2bd5450efcb78ef85c67e390
OHshajim <ajshajimmax@gmail.com>
2024-06-08 15:04:01 +0600

    photo host and camp post

--- src/Pages/DashboardPages/Organizer/AddCamp.jsx
+++ src/Pages/DashboardPages/Organizer/AddCamp.jsx
@@ -1,14 +1,43 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Shared/SectionTitle";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const AddCamp = () => {
+    const axiosPublic = useAxiosPublic()
+    const axiosSecure = useAxiosSecure()
+    const image_hosting_key = import.meta.env.VITE_PHOTO_HOSTING_KEY;
+    const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
     const {
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm() 
-     const onSubmit = (data) => console.log(data)
-
+    } = useForm()
+    const onSubmit = async (data) => {
+        console.log(data)
+        const imageFile = { image: data.photo[0] }
+        await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        })
+            .then(async (res) => {
+                console.log(res);
+                if (res.data.success) {
+                    const newCamp = {
+                        campName: data.campName,
+                        image: res.data.data.display_url,
+                        campFees: data.campFees,
+                        date: data.date,
+                        location: data.location,
+                        healthcareProfessionalName: data.professionalName,
+                        participantCount: data.participantCount,
+                        description: data.description
+                    }
+                    const res = await axiosSecure.post('/camps', newCamp)
+                }
+            })
+    }
     return (
         <div className="my-20">
             <SectionTitle subHeading="Adding " heading="Add a new Camp" />
@@ -19,15 +48,15 @@ const AddCamp = () => {
                         <div className='w-full md:w-1/2'>
                             <label className="font-semibold" >Camp Name</label>
                             <input {...register("campName", { required: true })}
-                             type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
-                              {errors.campName && <span  className="text-red-600 font-semibold text-sm">Camp Name is required***</span>}
+                                type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
+                            {errors.campName && <span className="text-red-600 font-semibold text-sm">Camp Name is required***</span>}
                         </div>
                         <div className='w-full md:w-1/2'>
                             <label className="font-semibold " >Healthcare Professional Name</label>
                             <input {...register("professionalName", { required: true })}
-                              type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
-                              {errors.professionalName && 
-                              <span className="text-red-600 font-semibold text-sm">Healthcare Professional Name is required***</span>}
+                                type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
+                            {errors.professionalName &&
+                                <span className="text-red-600 font-semibold text-sm">Healthcare Professional Name is required***</span>}
                         </div>
                     </div>
 
@@ -35,15 +64,15 @@ const AddCamp = () => {
                         <div className='w-full md:w-1/2'>
                             <label className="font-semibold " >Camp Fees</label>
                             <input {...register("campFees", { required: true })}
-                             type="number" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
-                             {errors.campFees && <span className="text-red-600 font-semibold text-sm">Camp Fees is required***</span>}
+                                type="number" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
+                            {errors.campFees && <span className="text-red-600 font-semibold text-sm">Camp Fees is required***</span>}
                         </div>
 
                         <div className='w-full md:w-1/2'>
                             <label className="font-semibold" >Location</label>
                             <input {...register("location", { required: true })}
-                             type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
-                             {errors.location && <span className="text-red-600 font-semibold text-sm">location is required***</span>}
+                                type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
+                            {errors.location && <span className="text-red-600 font-semibold text-sm">location is required***</span>}
                         </div>
                     </div>
 
@@ -51,29 +80,29 @@ const AddCamp = () => {
                         <div className='w-full md:w-1/2'>
                             <label className="font-semibold" >participant count</label>
                             <input {...register("participantCount", { required: true })}
-                             type="number" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
-                             {errors.participantCount && <span className="text-red-600 font-semibold text-sm">participant count is required***</span>}
+                                type="number" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
+                            {errors.participantCount && <span className="text-red-600 font-semibold text-sm">participant count is required***</span>}
                         </div>
                         <div className='w-full md:w-1/2'>
                             <label className="font-semibold " >Date</label>
                             <input {...register("date", { required: true })}
-                             type="date" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
-                             {errors.date && <span className="text-red-600 font-semibold text-sm">Date is required***</span>}
+                                type="date" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
+                            {errors.date && <span className="text-red-600 font-semibold text-sm">Date is required***</span>}
                         </div>
                     </div>
                     <div >
                         <label className="font-semibold" >Description</label>
                         <textarea {...register("description", { required: true })}
-                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
-                         {errors.description && <span className="text-red-600 font-semibold text-sm">Description is required***</span>}
+                            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring" />
+                        {errors.description && <span className="text-red-600 font-semibold text-sm">Description is required***</span>}
                     </div>
 
                     <div >
                         <label className="font-semibold " >Camp Photo</label> <br />
                         <input {...register("photo", { required: true })}
-                         type='file' className='mt-2' />
-                         {errors.photo &&
-                          <span className="text-red-600 font-semibold text-sm">Camp Photo is required***</span>}
+                            type='file' className='mt-2' />
+                        {errors.photo &&
+                            <span className="text-red-600 font-semibold text-sm">Camp Photo is required***</span>}
                     </div>
                 </div>
 
