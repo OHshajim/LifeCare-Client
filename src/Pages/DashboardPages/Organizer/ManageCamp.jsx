b29ed707fde154d2154a3de319432150f43729a5
OHshajim <ajshajimmax@gmail.com>
2024-06-08 20:21:17 +0600

    camps delete

--- src/Pages/DashboardPages/Organizer/ManageCamp.jsx
+++ src/Pages/DashboardPages/Organizer/ManageCamp.jsx
@@ -1,80 +1,100 @@
 import { FiDelete } from "react-icons/fi";
 import SectionTitle from "../../../Shared/SectionTitle";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+import { BsPencilSquare } from "react-icons/bs";
+import Swal from "sweetalert2";
 
 const ManageCamp = () => {
-
+    const axiosSecure = useAxiosSecure()
+    const axiosPublic = useAxiosPublic()
+    const { data: camps = [], refetch } = useQuery({
+        queryKey: ['camps'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/allCamps')
+            return res.data;
+        }
+    })
+    const handleDelete = async (id) => {
+        const res = await axiosSecure.delete(`/delete-camp/${id}`)
+        console.log(res);
+        if (res.data.deletedCount) {
+            Swal.fire({
+                title: 'Successfully Deleted',
+                text: 'Camp is successfully deleted !!!',
+                icon: "success"
+            });
+            refetch()
+        }
+    }
+    console.log(camps);
     return (
         <div>
             <SectionTitle subHeading="manage camps " heading="manage camps" />
             <div>
                 <section className="container px-4 mx-auto">
                     <div className="flex items-center gap-x-3">
-                        <h2 className="text-lg font-medium text-gray-800 dark:text-white">Team members</h2>
-
-                        <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">100 users</span>
+                        <h2 className="text-lg font-medium text-gray-800 Total camps"> Total Camps</h2>
+                        <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full  ">{camps.length} Camps</span>
                     </div>
 
                     <div className="flex flex-col mt-6">
                         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                                <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
-                                    <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                                        <thead className="bg-gray-50 dark:bg-gray-800">
+                                <div className="overflow-hidden border border-gray-200  md:rounded-lg">
+                                    <table className="min-w-full divide-y divide-gray-200 ">
+                                        <thead className="bg-gray-50 ">
                                             <tr>
-                                                <th scope="col" className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-
+                                                <th className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 ">
+                                                    Camp Name
                                                 </th>
 
-                                                <th scope="col" className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                                    <button className="flex items-center gap-x-2">
-                                                        <span>Status</span>
-                                                    </button>
+                                                <th className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 ">
+                                                    Date & Time
                                                 </th>
 
-                                                <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                                    <button className="flex items-center gap-x-2">
-                                                        <span>Role</span>
-
-
-                                                    </button>
+                                                <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 ">
+                                                    Location
                                                 </th>
 
-                                                <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">Email address</th>
+                                                <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 ">Healthcare Professional</th>
 
-                                                <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">Teams</th>
+                                                <th >
 
-                                                <th scope="col" className="relative py-3.5 px-4">
-                                                    <span className="sr-only">Edit</span>
                                                 </th>
                                             </tr>
                                         </thead>
-                                        <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-                                            <tr>
-                                                <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                                    <div className="inline-flex items-center gap-x-3">
-                                                    </div>
-                                                </td>
-                                                <td className="px-12 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                                    <div className="inline-flex items-center px-3 py-1 rounded-full gap-x-2 bg-emerald-100/60 dark:bg-gray-800">
-                                                        <span className="h-1.5 w-1.5 rounded-full bg-emerald-500"></span>
+                                        <tbody className="bg-white divide-y divide-gray-200  ">
+                                            {
+                                                camps.map(camp => <tr key={camp._id}>
+                                                    <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                                                        {camp.campName}
+                                                    </td>
+                                                    <td className="px-12 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                                                        {camp.date}
+                                                    </td>
+                                                    <td className="px-12 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                                                        {camp.location}
+                                                    </td>
+                                                    <td className="px-12 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                                                        {camp.healthcareProfessionalName}
+                                                    </td>
 
-                                                        <h2 className="text-sm font-normal text-emerald-500">Active</h2>
-                                                    </div>
-                                                </td>
-                                                <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Design Director</td>
-                                                <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">authurmelo@example.com</td>
-                                               
-                                                <td className="px-4 py-4 text-sm whitespace-nowrap">
-                                                    <div className="flex items-center gap-x-6">
-                                                        <button className="text-gray-500 transition-colors duration-200  hover:text-yellow-500 focus:outline-none">
-                                                        </button>
+                                                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                                                        <div className="flex items-center gap-x-6">
+                                                            <button className="text-gray-500 transition-colors duration-200  hover:text-blue-500 focus:outline-none text-xl">
+                                                                <BsPencilSquare />
+                                                            </button>
 
-                                                        <button className="text-gray-500 transition-colors duration-200  hover:text-red-500 focus:outline-none">
-                                                            <FiDelete />
-                                                        </button>
-                                                    </div>
-                                                </td>
-                                            </tr>
+                                                            <button onClick={() => handleDelete(camp._id)} className="text-gray-500 transition-colors duration-200  hover:text-red-500 focus:outline-none text-xl">
+                                                                <FiDelete />
+                                                            </button>
+                                                        </div>
+                                                    </td>
+                                                </tr>)
+                                            }
+
 
 
                                         </tbody>
@@ -85,9 +105,9 @@ const ManageCamp = () => {
                     </div>
 
                     <div className="flex items-center justify-between mt-6">
-                        <a href="#" className="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
+                        <a href="#" className="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100    -800">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5 rtl:-scale-x-100">
-                                <path  d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                                <path d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                             </svg>
 
                             <span>
@@ -96,18 +116,18 @@ const ManageCamp = () => {
                         </a>
 
                         <div className="items-center hidden lg:flex gap-x-3">
-                            <a href="#" className="px-2 py-1 text-sm text-blue-500 rounded-md dark:bg-gray-800 bg-blue-100/60">1</a>
-                            <a href="#" className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">2</a>
-                            <a href="#" className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">3</a>
+                            <a href="#" className="px-2 py-1 text-sm text-blue-500 rounded-md  bg-blue-100/60">1</a>
+                            <a href="#" className="px-2 py-1 text-sm text-gray-500 rounded-md -800  hover:bg-gray-100">2</a>
+                            <a href="#" className="px-2 py-1 text-sm text-gray-500 rounded-md -800  hover:bg-gray-100">3</a>
                         </div>
 
-                        <a href="#" className="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
+                        <a href="#" className="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100    -800">
                             <span>
                                 Next
                             </span>
 
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5 rtl:-scale-x-100">
-                                <path  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                                <path d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
                             </svg>
                         </a>
                     </div>
